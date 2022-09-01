33650ac4511c311d13be264e0bff4027d3e5afba
Do1e <dpj.email@qq.com>
2022-09-01 23:13:19 +0800

    修复大小写

--- decodeEnv.py
+++ decodeEnv.py
@@ -2,15 +2,15 @@ import os
 import json
 
 info = {}
-info['User_Agent'] = os.environ['User_Agent']
-info['Cookie'] = os.environ['Cookie']
-info['location'] = os.environ['location']
-info['body_temp_ok'] = os.environ['body_temp_ok']
-info['health_status'] = os.environ['health_status']
-info['my_health_code_color'] = os.environ['my_health_code_color']
-info['fam_mem_health_code_color'] = os.environ['fam_mem_health_code_color']
-info['last_RNA'] = os.environ['last_RNA']
-info['try_N_times'] = os.environ['try_N_times']
+info['USER_AGENT'] = os.environ['USER_AGENT']
+info['COOKIE'] = os.environ['COOKIE']
+info['LOCATION'] = os.environ['LOCATION']
+info['BODY_TEMP_OK'] = os.environ['BODY_TEMP_OK']
+info['HEALTH_STATUS'] = os.environ['HEALTH_STATUS']
+info['MY_HEALTH_CODE_COLOR'] = os.environ['MY_HEALTH_CODE_COLOR']
+info['FAM_MEM_HEALTH_CODE_COLOR'] = os.environ['FAM_MEM_HEALTH_CODE_COLOR']
+info['LAST_RNA'] = os.environ['LAST_RNA']
+info['TRY_N_TIMES'] = os.environ['TRY_N_TIMES']
 
 with open('config.json', 'w') as f:
     f.write(json.dumps(info, indent=4, ensure_ascii=False))
